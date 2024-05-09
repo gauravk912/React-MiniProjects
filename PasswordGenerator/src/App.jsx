36ed12faf71d8e6c6c8b0ada9c0f5410ab4fbdb9
Gaurav Kulkarni <gykulkarni23@gmail.com>
2024-05-09 16:04:18 +0530

    Password Generator

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,14 +1,98 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
+  //variables using UseState Hook
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useCallback is used to render a page auto upon any change
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_";
+    
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
   
+  // UseEffect Hook
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed,setPassword])
+
+  //useRef Hook
+  const passwordRef = useRef(null)
+
+  const copyPasswordToClipboard = useCallback(()=>[
+    passwordRef.current?.select(),
+    passwordRef.current?.setSelectionRange(0,99),
+    //can directly access window object as its react based on JS, cannot do the same in next js as it is server side rendering 
+    window.navigator.clipboard.writeText(password)
+  ],[password])
+
 
   return (
     <>
-      
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-10 bg-gray-800 text-orange-500">
+        <h1 className="text-white text-center my-3">Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 bg-blue-800 ">
+          <input
+            type="text"
+            value={password}
+            placeholder="Password"
+            className="outline-none w-full py-1 px-3"
+            readOnly
+            ref={passwordRef}
+          />
+          <button 
+          onClick={copyPasswordToClipboard}
+          className="px-3 text-white">Copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label>Length: {length}</label>
+          </div>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
